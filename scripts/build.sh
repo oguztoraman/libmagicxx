cfe853a00016cf06b9342cb196c5294c928c6843
Oğuz Toraman <oguz.toraman@tutanota.com>
2025-02-02 17:00:58 +0300

    Add rebuild the libmagic option.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,25 +10,28 @@ BUILD_DIR="release_build"
 BUILD_TYPE="Release"
 COMPILER="g++"
 RUN_TESTS="OFF"
+REBUILD_MAGIC="OFF"
 
 usage(){
-    echo "Usage: $0 [-d build_dir] [-b build_type] [-c compiler] [-t] [-h]"
+    echo "Usage: $0 [-d build_dir] [-b build_type] [-c compiler] [-t] [-r] [-h]"
     echo "  -d build_dir   Specify the build directory (default: ${BUILD_DIR})."
     echo "  -b build_type  Specify the CMake build type (default: ${BUILD_TYPE})."
     echo "  -c compiler    Specify the compiler (g++ or clang++, default: ${COMPILER})."
     echo "  -t             Build and run tests (default: ${RUN_TESTS})."
+    echo "  -r             Rebuild libmagic (default: ${REBUILD_MAGIC})."
     echo "  -h             Display this message."
     exit 1
 }
 
 DISPLAY_USAGE=true
 
-while getopts 'd:b:c:ht' OPTION; do
+while getopts 'd:b:c:htr' OPTION; do
     case ${OPTION} in
         d) BUILD_DIR=$OPTARG  DISPLAY_USAGE=false;;
         b) BUILD_TYPE=$OPTARG DISPLAY_USAGE=false;;
         c) COMPILER=$OPTARG   DISPLAY_USAGE=false;;
         t) RUN_TESTS="ON"     DISPLAY_USAGE=false;;
+        r) REBUILD_MAGIC="ON" DISPLAY_USAGE=false;;
         *) usage;;
     esac
 done
@@ -37,9 +40,9 @@ if [ "$DISPLAY_USAGE" = true ] && [ $# -gt 0 ]; then
     usage
 fi
 
-echo "Selected options: build_dir=${BUILD_DIR}, build_type=${BUILD_TYPE}, compiler=${COMPILER}, build and run tests=${RUN_TESTS}"
+echo "Selected options: build_dir=${BUILD_DIR}, build_type=${BUILD_TYPE}, compiler=${COMPILER}, build and run tests=${RUN_TESTS}, rebuild libmagic=${REBUILD_MAGIC}"
 
-cmake -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DBUILD_MAGICXX_TESTS=${RUN_TESTS} -DCMAKE_CXX_COMPILER:FILEPATH=${COMPILER} -G Ninja -S . -B ${BUILD_DIR} || {
+cmake -DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} -DBUILD_MAGICXX_TESTS=${RUN_TESTS} -DREBUILD_MAGIC=${REBUILD_MAGIC} -DCMAKE_CXX_COMPILER:FILEPATH=${COMPILER} -G Ninja -S . -B ${BUILD_DIR} || {
     exit 2
 }
 

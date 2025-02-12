ff5d7f74db70a09df1cff766c61387eb05f13aa7
Oğuz Toraman <oguz.toraman@tutanota.com>
2025-02-12 21:41:22 +0300

    Use set -e in install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,14 +3,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025 Oğuz Toraman <oguz.toraman@tutanota.com>
 # SPDX-License-Identifier: LGPL-3.0-only
 
-which dnf &> /dev/null || {
-    echo "Error: Unsupported system. DNF does not exist on this system."
-    echo "You need to install the dependencies manually."
-    exit 1
-}
+set -e
 
 echo "Installing the dependencies..."
-sudo dnf install -y cmake make ninja-build g++ clang clang-tools-extra libcxx-devel git autoconf libtool doxygen || {
-    exit 2
-}
+sudo dnf install -y cmake make ninja-build g++ clang clang-tools-extra libcxx-devel git autoconf libtool doxygen
 echo "Done"

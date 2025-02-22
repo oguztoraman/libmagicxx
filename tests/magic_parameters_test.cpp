fb61b12ddd050aa8adaa0e68fa0a03591aa2a092
Oğuz Toraman <oguz.toraman@tutanota.com>
2025-02-22 20:19:47 +0300

    Use using enum and structured binding declarations for magic flags and parameters in tests for improved readability.

--- tests/magic_parameters_test.cpp
+++ tests/magic_parameters_test.cpp
@@ -29,20 +29,21 @@ TEST(magic_parameters_test, opened_magic_set_parameter_get_parameter)
     m.open(magic::flags::mime);
     std::mt19937                               eng{std::random_device{}()};
     std::uniform_int_distribution<std::size_t> dist{0, 100};
-    magic::parameter_value_map_t               expected_parameters{
-                      {magic::parameters::indir_max,      dist(eng)},
-                      {magic::parameters::name_max,       dist(eng)},
-                      {magic::parameters::elf_phnum_max,  dist(eng)},
-                      {magic::parameters::elf_shnum_max,  dist(eng)},
-                      {magic::parameters::elf_notes_max,  dist(eng)},
-                      {magic::parameters::regex_max,      dist(eng)},
-                      {magic::parameters::bytes_max,      dist(eng)},
-                      {magic::parameters::encoding_max,   dist(eng)},
-                      {magic::parameters::elf_shsize_max, dist(eng)}
+    using enum magic::parameters;
+    magic::parameter_value_map_t expected_parameters{
+        {indir_max,      dist(eng)},
+        {name_max,       dist(eng)},
+        {elf_phnum_max,  dist(eng)},
+        {elf_shnum_max,  dist(eng)},
+        {elf_notes_max,  dist(eng)},
+        {regex_max,      dist(eng)},
+        {bytes_max,      dist(eng)},
+        {encoding_max,   dist(eng)},
+        {elf_shsize_max, dist(eng)}
     };
-    for (const auto& parameter_pair : expected_parameters) {
-        m.set_parameter(parameter_pair.first, parameter_pair.second);
-        EXPECT_EQ(m.get_parameter(parameter_pair.first), parameter_pair.second);
+    for (const auto& [parameter, value] : expected_parameters) {
+        m.set_parameter(parameter, value);
+        EXPECT_EQ(m.get_parameter(parameter), value);
     }
     EXPECT_EQ(expected_parameters, m.get_parameters());
     m.open(magic::flags::mime);
@@ -52,35 +53,37 @@ TEST(magic_parameters_test, opened_magic_set_parameter_get_parameter)
 
 TEST(magic_parameters_test, parameter_to_string_conversion)
 {
+    using enum magic::parameters;
     std::vector magic_parameters_with_names{
-        std::make_pair(magic::parameters::indir_max, "indir_max"),
-        std::make_pair(magic::parameters::name_max, "name_max"),
-        std::make_pair(magic::parameters::elf_phnum_max, "elf_phnum_max"),
-        std::make_pair(magic::parameters::elf_shnum_max, "elf_shnum_max"),
-        std::make_pair(magic::parameters::elf_notes_max, "elf_notes_max"),
-        std::make_pair(magic::parameters::regex_max, "regex_max"),
-        std::make_pair(magic::parameters::bytes_max, "bytes_max"),
-        std::make_pair(magic::parameters::encoding_max, "encoding_max"),
-        std::make_pair(magic::parameters::elf_shsize_max, "elf_shsize_max")
+        std::make_pair(indir_max, "indir_max"),
+        std::make_pair(name_max, "name_max"),
+        std::make_pair(elf_phnum_max, "elf_phnum_max"),
+        std::make_pair(elf_shnum_max, "elf_shnum_max"),
+        std::make_pair(elf_notes_max, "elf_notes_max"),
+        std::make_pair(regex_max, "regex_max"),
+        std::make_pair(bytes_max, "bytes_max"),
+        std::make_pair(encoding_max, "encoding_max"),
+        std::make_pair(elf_shsize_max, "elf_shsize_max")
     };
-    for (const auto& parameter_pair : magic_parameters_with_names) {
-        EXPECT_EQ(to_string(parameter_pair.first), parameter_pair.second);
+    for (const auto& [parameter, parameter_str] : magic_parameters_with_names) {
+        EXPECT_EQ(to_string(parameter), parameter_str);
     }
 }
 
 TEST(magic_parameters_test, parameters_to_string_conversion)
 {
+    using enum magic::parameters;
     EXPECT_EQ(
         to_string(magic::parameter_value_map_t{
-            {magic::parameters::indir_max,      1},
-            {magic::parameters::name_max,       2},
-            {magic::parameters::elf_phnum_max,  3},
-            {magic::parameters::elf_shnum_max,  4},
-            {magic::parameters::elf_notes_max,  5},
-            {magic::parameters::regex_max,      6},
-            {magic::parameters::bytes_max,      7},
-            {magic::parameters::encoding_max,   8},
-            {magic::parameters::elf_shsize_max, 9}
+            {indir_max,      1},
+            {name_max,       2},
+            {elf_phnum_max,  3},
+            {elf_shnum_max,  4},
+            {elf_notes_max,  5},
+            {regex_max,      6},
+            {bytes_max,      7},
+            {encoding_max,   8},
+            {elf_shsize_max, 9}
     }),
         "indir_max: 1, "
         "name_max: 2, "

d5886778120e901442d502abf56948079385a4cd
Oğuz Toraman <oguz.toraman@tutanota.com>
2025-05-24 12:09:59 +0000

    CMake: Add option to disable installation of Libmagicxx.

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -30,76 +30,78 @@ if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
     )
 endif()
 
-install(
-    FILES
-        ${magicxx_DEFAULT_DATABASE_FILES}
-    DESTINATION
-        ${magicxx_INSTALL_DEFAULT_DATABASES_DIR}
-    COMPONENT
-        Data
-)
+if (INSTALL_MAGICXX)
+    install(
+        FILES
+            ${magicxx_DEFAULT_DATABASE_FILES}
+        DESTINATION
+            ${magicxx_INSTALL_DEFAULT_DATABASES_DIR}
+        COMPONENT
+            Data
+    )
 
-install(
-    FILES
-        ${magicxx_HEADER_FILES}
-    DESTINATION
-        ${magicxx_INSTALL_INCLUDE_DIR}
-    COMPONENT
-        Development
-)
+    install(
+        FILES
+            ${magicxx_HEADER_FILES}
+        DESTINATION
+            ${magicxx_INSTALL_INCLUDE_DIR}
+        COMPONENT
+            Development
+    )
 
-install(
-    EXPORT
-        magicxxTargets
-    NAMESPACE
-        recognition::
-    DESTINATION
-        ${magicxx_CMAKE_INSTALL_LIB_DIR}
-    COMPONENT
-        Development
-)
+    install(
+        EXPORT
+            magicxxTargets
+        NAMESPACE
+            recognition::
+        DESTINATION
+            ${magicxx_CMAKE_INSTALL_LIB_DIR}
+        COMPONENT
+            Development
+    )
 
-install(
-    FILES
-        ${magicxx_CMAKE_PACKAGE_CONFIG_FILE}
-        ${magicxx_CMAKE_PACKAGE_CONFIG_VERSION_FILE}
-    DESTINATION
-        ${magicxx_CMAKE_INSTALL_LIB_DIR}
-    COMPONENT
-        Development
-)
+    install(
+        FILES
+            ${magicxx_CMAKE_PACKAGE_CONFIG_FILE}
+            ${magicxx_CMAKE_PACKAGE_CONFIG_VERSION_FILE}
+        DESTINATION
+            ${magicxx_CMAKE_INSTALL_LIB_DIR}
+        COMPONENT
+            Development
+    )
 
-install(
-    TARGETS
-        magicxx
-    EXPORT
-        magicxxTargets
-    FILE_SET
-        magicxxHeaders
-    LIBRARY DESTINATION
-        ${magicxx_INSTALL_LIB_DIR}
-    COMPONENT
-        Library
-)
+    install(
+        TARGETS
+            magicxx
+        EXPORT
+            magicxxTargets
+        FILE_SET
+            magicxxHeaders
+        LIBRARY DESTINATION
+            ${magicxx_INSTALL_LIB_DIR}
+        COMPONENT
+            Library
+    )
 
-install(
-    TARGETS
-        magicxx_static
-    EXPORT
-        magicxxTargets
-    FILE_SET
-        magicxx_staticHeaders
-    ARCHIVE DESTINATION
-        ${magicxx_INSTALL_LIB_DIR}
-    COMPONENT
-        Archive
-)
+    install(
+        TARGETS
+            magicxx_static
+        EXPORT
+            magicxxTargets
+        FILE_SET
+            magicxx_staticHeaders
+        ARCHIVE DESTINATION
+            ${magicxx_INSTALL_LIB_DIR}
+        COMPONENT
+            Archive
+    )
 
-install(
-    DIRECTORY
-        documentation/html
-    DESTINATION
-        ${magicxx_INSTALL_DOC_DIR}
-    COMPONENT
-        Documentation
-)
+    install(
+        DIRECTORY
+            documentation/html
+        DESTINATION
+            ${magicxx_INSTALL_DOC_DIR}
+        COMPONENT
+            Documentation
+    )
+endif(INSTALL_MAGICXX)
